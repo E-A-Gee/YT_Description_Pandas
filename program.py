50a8a257e4b85d7081e8731fe003c33968221fd0
E-A-Gee <70771300+E-A-Gee@users.noreply.github.com>
2021-02-07 20:05:31 -0500

    add YouTube title capability

--- program.py
+++ program.py
@@ -3,9 +3,19 @@ import pandas as pd
 filepath = input(
     'Please enter the filepath of the file you\'d like to use: \n')
 
+# Allow people to type the filepath within quotes
+if filepath[0] == '"' and filepath[-1] == '"':
+    filepath = filepath[1:-1]
+
 filepath = filepath.replace('\\', '\\\\')
 df = pd.read_csv(filepath, encoding="utf-8-sig")
 
+# Add Title Column
+df['youtube_title'] = df.apply(
+    lambda row: f'{row.title} ({row.sku}) by {row.composer}' if str(row.arranger)=='nan'\
+  else f'{row.title} ({row.sku}) arr. {row.arranger}', axis=1)
+
+# Description Column
 df['youtube_description'] = df.apply(
     lambda row: f'To purchase or for more information: http://www.mymusicpublisher.com/{row.sku}\
 \n\nGrade {row.grade} {row.ensemble}\
